b1c5884c5aed1f6e421cdb5db0e656efb90ba4fb
Blubbyo <137796376+Blubbyo@users.noreply.github.com>
2025-07-02 10:01:32 +0200

    Add files via upload

--- js/firestore-preise.js
+++ js/firestore-preise.js
@@ -1,12 +1,34 @@
 ﻿// js/firestore-preise.js
 import { db } from './firebase-init.js';
-import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+//import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
 
-export async function loadPrices() {
-  const docSnap = await getDoc(doc(db, "preisListe", "daten"));
-  return docSnap.exists() ? docSnap.data().einträge || [] : [];
+
+//export async function loadPrices() {
+ // const docSnap = await getDoc(doc(db, "preisListe", "daten"));
+ // return docSnap.exists() ? docSnap.data().einträge || [] : [];
+//}
+
+//export async function savePrices(data) {
+//  await setDoc(doc(db, "preisListe", "daten"), { einträge: data });
+//}
+
+
+async function loadPrices() {
+  const dbRef = ref(db, "preisListe");
+  const snapshot = await get(dbRef);
+  if (snapshot.exists()) {
+    const data = snapshot.val(); // gibt ein Objekt oder Array zurück
+    // Falls Object, ggf. in Array umwandeln:
+    return Object.values(data);
+  } else {
+    return [];
+  }
 }
 
-export async function savePrices(data) {
-  await setDoc(doc(db, "preisListe", "daten"), { einträge: data });
+function savePrices(data) {
+  // data ist ein Array von Preisobjekten
+  set(ref(db, "preisListe"), data)
+    .then(() => console.log("Preise gespeichert"))
+    .catch(err => console.error(err));
 }
